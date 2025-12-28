b39c0a04248753baef28a4df7b1509886af61d85
eaglesruleder <44625326+eaglesruleder@users.noreply.github.com>
2025-12-28 21:31:28 +0800

    Initial model definition

--- OddWire-Timberworks/OddWireTimberworks/OddWireTimberworksModSystem.cs
+++ OddWire-Timberworks/OddWireTimberworks/OddWireTimberworksModSystem.cs
@@ -1,8 +1,13 @@
 ﻿using Vintagestory.API.Common;
 
-namespace OddWireTimberworks
+namespace OddWire
 {
     public class OddWireTimberworksModSystem : ModSystem
     {
+        public override void Start(ICoreAPI api)
+        {
+        //  api.RegisterBlockClass(Mod.Info.ModID+".", typeof());
+            base.Start(api);
+        }
     }
 }
