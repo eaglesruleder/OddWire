0a86da798781dd56a425971c46aaf6c0d093d46b
eaglesruleder <44625326+eaglesruleder@users.noreply.github.com>
2025-12-30 22:17:15 +0800

    Corrections

---
+++ OddWire/OddWire/BlockEntity/Brazier/BrazierInterfaces.cs
@@ -0,0 +1,76 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
+
+#nullable disable
+
+namespace OddWire.GameContent
+{
+    public interface IBrazier
+    {
+        bool IsBurning { get; }
+    }
+
+    public enum EnumBrazierModel
+    {
+        Normal = 0,
+        Spit = 1,
+        Wide = 2
+    }
+
+    public interface IInBrazierMeshSupplier
+    {
+        /// <summary>
+        /// Return the mesh you want to be rendered in the brazier. You can return null to signify that you do not wish to use a custom mesh.
+        /// </summary>
+        /// <param name="stack"></param>
+        /// <param name="world"></param>
+        /// <param name="pos"></param>
+        /// <param name="brazierModel"></param>
+        /// <returns></returns>
+        MeshData GetMeshWhenInBrazier(ItemStack stack, IWorldAccessor world, BlockPos pos, ref EnumBrazierModel brazierModel);
+    }
+
+    public class InBrazierProps
+    {
+        public ModelTransform Transform;
+        public EnumBrazierModel UseBrazierModel;
+    }
+
+    public interface IInBrazierRenderer : IRenderer
+    {
+        /// <summary>
+        /// Called every 100ms in case you want to do custom stuff, such as playing a sound after a certain temperature
+        /// </summary>
+        /// <param name="temperature"></param>
+        void OnUpdate(float temperature);
+
+        /// <summary>
+        /// Called when the itemstack has been moved to the output slot
+        /// </summary>
+        void OnCookingComplete();
+    }
+
+    public interface IInBrazierRendererSupplier
+    {
+        /// <summary>
+        /// Return the renderer that perfroms the rendering of your block/item in the brazier. You can return null to signify that you do not wish to use a custom renderer
+        /// </summary>
+        /// <param name="stack"></param>
+        /// <param name="world"></param>
+        /// <param name="pos"></param>
+        /// <returns></returns>
+        IInBrazierRenderer GetRendererWhenInBrazier(ItemStack stack, BlockEntityBrazier brazier, bool forOutputSlot);
+
+        /// <summary>
+        /// The model type the brazier should be using while you render your custom item
+        /// </summary>
+        /// <param name="stack"></param>
+        /// <param name="world"></param>
+        /// <param name="pos"></param>
+        /// <returns></returns>
+        EnumBrazierModel GetDesiredBrazierModel(ItemStack stack, BlockEntityBrazier brazier, bool forOutputSlot);
+    }
+
+}
---
+++ OddWire/OddWire/OddWireModSystem.cs
@@ -0,0 +1,15 @@
+﻿using OddWire.GameContent;
+using Vintagestory.API.Common;
+
+namespace OddWire
+{
+    public class OddWireModSystem : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            api.RegisterBlockClass(Mod.Info.ModID+".", typeof(BlockBrazier));
+            api.RegisterBlockEntityClass(Mod.Info.ModID+".", typeof(BlockEntityBrazier));
+            base.Start(api);
+        }
+    }
+}
