5e3841b41c93bf7c0c5ab85ade2251ed8cac2f6b
eaglesruleder <44625326+eaglesruleder@users.noreply.github.com>
2026-01-01 08:32:11 +0800

    Load brazier fabrication recipes

--- OddWire/OddWire/BlockEntityRenderer/BrazierContentsRenderer.cs
+++ OddWire/OddWire/BlockEntityRenderer/BrazierContentsRenderer.cs
@@ -10,6 +10,7 @@ namespace OddWire.GameContent
     public class BrazierContentsRenderer : IRenderer
     {
         MultiTextureMeshRef meshref;
+        MultiTextureMeshRef[] craftingMeshRefs;
         ICoreClientAPI api;
         BlockPos pos;
         public ItemStack ContentStack;
@@ -18,6 +19,8 @@ namespace OddWire.GameContent
 
         ModelTransform transform;
         ModelTransform defaultTransform;
+        ItemStack[] craftingStacks;
+        Vec3f[] craftingOffsets;
 
         public IInBrazierRenderer contentStackRenderer;
         public bool RequireSpit
@@ -66,14 +69,58 @@ namespace OddWire.GameContent
             this.ContentStack = contentStack;
             meshref?.Dispose();
             meshref = null;
+            ClearCraftingMeshes();
             
             contentStackRenderer = renderer;
+        }
+
+        internal void SetCraftingSteps(ItemStack[] steps)
+        {
+            contentStackRenderer?.Dispose();
+            contentStackRenderer = null;
+
+            meshref?.Dispose();
+            meshref = null;
+
+            ClearCraftingMeshes();
+
+            if (steps == null || steps.Length == 0)
+            {
+                ContentStack = null;
+                return;
+            }
+
+            craftingStacks = steps;
+            craftingMeshRefs = new MultiTextureMeshRef[steps.Length];
+            craftingOffsets = new Vec3f[steps.Length];
+
+            for (int index = 0; index < steps.Length; index++)
+            {
+                ItemStack stack = steps[index];
+                if (stack == null) continue;
+
+                MeshData ingredientMesh;
+                if (stack.Class == EnumItemClass.Item)
+                {
+                    api.Tesselator.TesselateItem(stack.Item, out ingredientMesh);
+                }
+                else
+                {
+                    api.Tesselator.TesselateBlock(stack.Block, out ingredientMesh);
+                }
+
+                craftingMeshRefs[index] = api.Render.UploadMultiTextureMesh(ingredientMesh);
+                craftingOffsets[index] = GetCraftingOffset(index, steps.Length);
+            }
+
+            ContentStack = steps[0];
         }
 
         public void SetContents(ItemStack newContentStack, ModelTransform transform)
         {
             contentStackRenderer?.Dispose();
             contentStackRenderer = null;
+            ClearCraftingMeshes();
 
             this.transform = transform;
             if (transform == null) this.transform = defaultTransform;
@@ -104,6 +151,20 @@ namespace OddWire.GameContent
             this.ContentStack = newContentStack;
         }
 
+        void ClearCraftingMeshes()
+        {
+            if (craftingMeshRefs == null) return;
+
+            for (int i = 0; i < craftingMeshRefs.Length; i++)
+            {
+                craftingMeshRefs[i]?.Dispose();
+            }
+
+            craftingMeshRefs = null;
+            craftingStacks = null;
+            craftingOffsets = null;
+        }
+
         public void OnRenderFrame(float deltaTime, EnumRenderStage stage)
         {
             if (contentStackRenderer != null)
@@ -112,7 +173,7 @@ namespace OddWire.GameContent
                 return;
             }
 
-            if (meshref == null) return;
+            if (meshref == null && (craftingMeshRefs == null || craftingMeshRefs.Length == 0)) return;
             
             IRenderAPI rpi = api.Render;
             Vec3d camPos = api.World.Player.Entity.CameraPos;
@@ -135,35 +196,79 @@ namespace OddWire.GameContent
             prog.AlphaTest = 0.05f;
             prog.OverlayOpacity = 0;
 
-            int temp = (int)ContentStack.Collectible.GetTemperature(api.World, ContentStack);
             Vec4f lightrgbs = api.World.BlockAccessor.GetLightRGBs(pos.X, pos.Y, pos.Z);
-            float[] glowColor = ColorUtil.GetIncandescenceColorAsColor4f(temp);
-            lightrgbs[0] += glowColor[0];
-            lightrgbs[1] += glowColor[1];
-            lightrgbs[2] += glowColor[2];
 
-            prog.RgbaLightIn = lightrgbs;
-            
-            prog.ExtraGlow = (int)GameMath.Clamp((temp - 500) / 4, 0, 255);
-            
-            prog.ModelMatrix = ModelMat
-                .Identity()
-                .Translate(pos.X - camPos.X + transform.Translation.X, pos.Y - camPos.Y + transform.Translation.Y, pos.Z - camPos.Z + transform.Translation.Z)
-                .Translate(transform.Origin.X, 0.6f + transform.Origin.Y, transform.Origin.Z)
-                .RotateX(transform.Rotation.X * GameMath.DEG2RAD)
-                .RotateY(transform.Rotation.Y * GameMath.DEG2RAD)
-                .RotateZ(transform.Rotation.Z * GameMath.DEG2RAD)
-                .Scale(transform.ScaleXYZ.X, transform.ScaleXYZ.Y, transform.ScaleXYZ.Z)
-                .Translate(-transform.Origin.X, -transform.Origin.Y, -transform.Origin.Z)
-                .Values
-            ;
+            if (meshref != null)
+            {
+                int temp = (int)ContentStack.Collectible.GetTemperature(api.World, ContentStack);
+                float[] glowColor = ColorUtil.GetIncandescenceColorAsColor4f(temp);
+                lightrgbs[0] += glowColor[0];
+                lightrgbs[1] += glowColor[1];
+                lightrgbs[2] += glowColor[2];
 
-            prog.ViewMatrix = rpi.CameraMatrixOriginf;
-            prog.ProjectionMatrix = rpi.CurrentProjectionMatrix;
+                prog.RgbaLightIn = lightrgbs;
+                prog.ExtraGlow = (int)GameMath.Clamp((temp - 500) / 4, 0, 255);
 
-            rpi.RenderMultiTextureMesh(meshref, "tex");
+                prog.ModelMatrix = ModelMat
+                    .Identity()
+                    .Translate(pos.X - camPos.X + transform.Translation.X, pos.Y - camPos.Y + transform.Translation.Y, pos.Z - camPos.Z + transform.Translation.Z)
+                    .Translate(transform.Origin.X, 0.6f + transform.Origin.Y, transform.Origin.Z)
+                    .RotateX(transform.Rotation.X * GameMath.DEG2RAD)
+                    .RotateY(transform.Rotation.Y * GameMath.DEG2RAD)
+                    .RotateZ(transform.Rotation.Z * GameMath.DEG2RAD)
+                    .Scale(transform.ScaleXYZ.X, transform.ScaleXYZ.Y, transform.ScaleXYZ.Z)
+                    .Translate(-transform.Origin.X, -transform.Origin.Y, -transform.Origin.Z)
+                    .Values
+                ;
+
+                prog.ViewMatrix = rpi.CameraMatrixOriginf;
+                prog.ProjectionMatrix = rpi.CurrentProjectionMatrix;
+
+                rpi.RenderMultiTextureMesh(meshref, "tex");
+            }
+
+            if (craftingMeshRefs != null)
+            {
+                for (int i = 0; i < craftingMeshRefs.Length; i++)
+                {
+                    MultiTextureMeshRef mesh = craftingMeshRefs[i];
+                    ItemStack stack = craftingStacks?[i];
+                    if (mesh == null || stack == null) continue;
+
+                    int temp = (int)stack.Collectible.GetTemperature(api.World, stack);
+                    float[] glowColor = ColorUtil.GetIncandescenceColorAsColor4f(temp);
+                    Vec4f stackLight = new Vec4f(lightrgbs[0] + glowColor[0], lightrgbs[1] + glowColor[1], lightrgbs[2] + glowColor[2], lightrgbs[3]);
+
+                    prog.RgbaLightIn = stackLight;
+                    prog.ExtraGlow = (int)GameMath.Clamp((temp - 500) / 4, 0, 255);
+
+                    Vec3f offset = craftingOffsets[i];
+                    prog.ModelMatrix = ModelMat
+                        .Identity()
+                        .Translate(pos.X - camPos.X + offset.X, pos.Y - camPos.Y + offset.Y, pos.Z - camPos.Z + offset.Z)
+                        .Scale(0.25f, 0.25f, 0.25f)
+                        .Values
+                    ;
+
+                    prog.ViewMatrix = rpi.CameraMatrixOriginf;
+                    prog.ProjectionMatrix = rpi.CurrentProjectionMatrix;
+
+                    rpi.RenderMultiTextureMesh(mesh, "tex");
+                }
+            }
 
             prog.Stop();
+        }
+
+        Vec3f GetCraftingOffset(int index, int total)
+        {
+            if (total <= 1) return new Vec3f(0.5f, 0.5f, 0.5f);
+
+            float angle = (float)(index * GameMath.TWOPI / total);
+            float radius = 0.18f;
+            float x = 0.5f + GameMath.Cos(angle) * radius;
+            float z = 0.5f + GameMath.Sin(angle) * radius;
+            return new Vec3f(x, 0.5f, z);
         }
 
         public void Dispose()
@@ -171,6 +276,7 @@ namespace OddWire.GameContent
             api.Event.UnregisterRenderer(this, EnumRenderStage.Opaque);
 
             meshref?.Dispose();
+            ClearCraftingMeshes();
             contentStackRenderer?.Dispose();
         }
 
--- OddWire/OddWire/OddWireModSystem.cs
+++ OddWire/OddWire/OddWireModSystem.cs
@@ -5,10 +5,13 @@ namespace OddWire
 {
     public class OddWireModSystem : ModSystem
     {
+        public FabricationRecipeManager FabricationRecipes { get; private set; }
+
         public override void Start(ICoreAPI api)
         {
             api.RegisterBlockClass(Mod.Info.ModID+".", typeof(BlockBrazier));
             api.RegisterBlockEntityClass(Mod.Info.ModID+".", typeof(BlockEntityBrazier));
+            FabricationRecipes = new FabricationRecipeManager(api);
             base.Start(api);
         }
     }
