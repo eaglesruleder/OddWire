c23ec66e5d43862296a5b7609859cb02253d818d
eaglesruleder <44625326+eaglesruleder@users.noreply.github.com>
2026-01-20 18:36:47 +0800

    CacheMesh now render qtyRoot elements

--- OddWire/OddWire/_Extensions/VintageStory/API.Common/BlockEntityExtensions.cs
+++ OddWire/OddWire/_Extensions/VintageStory/API.Common/BlockEntityExtensions.cs
@@ -8,9 +8,9 @@ namespace OddWire.VintageStory.API.Common
 {
     public static class BlockEntityExtensions
     {
-        public static bool CacheMesh(this BlockEntity blockEntity, string path, string cacheKey, out MeshData meshdata, int? quantityElements = null, Action<MeshData> onCreate = null)
+        public static bool CacheMesh(this BlockEntity blockEntity, string path, string cacheKey, out MeshData meshdata, int? qtyRootElements = null, Action<MeshData> onCreate = null)
         {
-            string meshKey = $"{path}#{quantityElements}";
+            string meshKey = $"{path}#{qtyRootElements}";
             Dictionary<string, MeshData> meshes = ObjectCacheUtil.GetOrCreate(blockEntity.Api, cacheKey, () => new Dictionary<string, MeshData>());
             if (meshes.TryGetValue(meshKey, out meshdata))
                 return true;
@@ -22,12 +22,33 @@ namespace OddWire.VintageStory.API.Common
             Shape shape = Shape.TryGet(blockEntity.Api, $"{path}.json");
             if (shape == null)
                 return false;
+
+            //  Apply quantityElements as qty Root, not qty Nested
+            int? renderElements = null;
+            if (qtyRootElements != null)
+            {
+                renderElements = 0;
+                for (int i = 0; i < qtyRootElements.Value && i < shape.Elements.Length; i++)
+                    renderElements += 1 + CountElementChildren(shape.Elements[i].Children);
+            }
             
             ITesselatorAPI mesher = ((ICoreClientAPI)blockEntity.Api).Tesselator;
-            mesher.TesselateShape(block, shape, out meshdata, quantityElements: quantityElements);
+            mesher.TesselateShape(block, shape, out meshdata, quantityElements: renderElements);
             onCreate?.Invoke(meshdata);
             meshes.TryAdd(meshKey, meshdata);
             return true;
+        }
+
+        private static int CountElementChildren(ShapeElement[] children)
+        {
+            if (children == null || children.Length == 0)
+                return 0;
+            
+            int result = 0;
+            for (int i = 0; i < children.Length; i++)
+                result += 1 + CountElementChildren(children[i].Children);
+            
+            return result;
         }
     }
 }
