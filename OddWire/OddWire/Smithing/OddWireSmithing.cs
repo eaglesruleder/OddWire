78cc30bfd4772a6e42a9a6356697d9479d1f58ce
eaglesruleder <44625326+eaglesruleder@users.noreply.github.com>
2026-02-01 17:30:24 +0800

    Namespace changes

---
+++ OddWire/OddWire/Smithing/OddWireSmithing.cs
@@ -0,0 +1,16 @@
+﻿using OddWire.GameContent;
+using Vintagestory.API.Common;
+
+namespace OddWire
+{
+    public class OddWireSmithing : ModSystem
+    {
+        public override void Start(ICoreAPI api)
+        {
+            api.RegisterBlockClass($"{Mod.Info.ModID}.holysymbol", typeof(BlockBrazier));
+            api.RegisterBlockEntityClass($"{Mod.Info.ModID}smithing.brazier.blockentity", typeof(BlockEntityBrazier));
+            
+            base.Start(api);
+        }
+    }
+}
