642f748dee7ad19905a6d6a4ab09b3d62b4d91d5
eaglesruleder <44625326+eaglesruleder@users.noreply.github.com>
2026-02-08 10:04:15 +0800

    Add harmony bootstrap for barrel patch

---
+++ OddWire/OddWire/OddWireHarmony.cs
@@ -0,0 +1,23 @@
+using HarmonyLib;
+using Vintagestory.API.Common;
+
+namespace OddWire;
+
+public class OddWireHarmony : ModSystem
+{
+    private Harmony? harmony;
+
+    public override void Start(ICoreAPI api)
+    {
+        harmony = new Harmony("oddwire.harmony");
+        harmony.PatchAll();
+        base.Start(api);
+    }
+
+    public override void Dispose()
+    {
+        harmony?.UnpatchAll(harmony.Id);
+        harmony = null;
+        base.Dispose();
+    }
+}
