0584b999eb25adcba795e1b5d9e5927b49cc770d
eaglesruleder <44625326+eaglesruleder@users.noreply.github.com>
2026-02-13 09:39:59 +0800

    Add Browns and Inoculum

--- OddWire/OddWire/Farming/CompostPile/BECompostpile.cs
+++ OddWire/OddWire/Farming/CompostPile/BECompostpile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Vintagestory.API.Common;
 using Vintagestory.API.Datastructures;
@@ -6,6 +7,8 @@ namespace OddWire.GameContent;
 public class BlockEntityCompostPile : BlockEntity
 {
     private const int MAX_STACK_SIZE = 64;
+    private const int SOUR_PER_INOCULUM = 2;
+    private const int ROT_PER_INOCULUM = 4;
     
     private int _brownsQty = 0;
     private int _inoculumQty = 0;
@@ -43,8 +46,14 @@ public class BlockEntityCompostPile : BlockEntity
         if (slot.StackSize < 1)
             return false;
 
-        if (TryAddNutrition(slot, out accepted))
+        if (TryAddNutrition(slot, out accepted)
+        ||  TryAddBrowns(slot, out accepted)
+        ||  TryAddInoculum(slot, out accepted)
+            )
+        {
+            MarkDirty(true);
             return accepted > 0;
+        }
         
         return false;
     }
@@ -62,52 +71,111 @@ public class BlockEntityCompostPile : BlockEntity
         int room = MAX_STACK_SIZE - NutritionQty;
         if(room < 1)
             return false;
-
-        int actualStackSize = slot.StackSize;
-        int adjustedStackSize = actualStackSize;
-        int adjustedStackRatio = 1;
-        if (slot.MaxSlotStackSize != 64)
-        {
-            adjustedStackRatio = 64 / slot.MaxSlotStackSize;
-            adjustedStackSize = actualStackSize * adjustedStackRatio;
-        }
-
-        while
-           (actualStackSize > 0 
-        &&  adjustedStackSize > room
-            )
-        {
-            actualStackSize--;
-            adjustedStackSize -= adjustedStackRatio;
-        }
         
-        if(actualStackSize < 1)
+        int ratio = 1;
+        if (slot.MaxSlotStackSize != 64)
+            ratio = Math.Max(64 / slot.MaxSlotStackSize, 1);
+        
+        if (slot.StackSize < ratio)
             return false;
+                
+        int adjustedStackSize = slot.StackSize / ratio;
+        int adjustedAccept = adjustedStackSize > room 
+        ?   room
+        :   adjustedStackSize;
         
         _nutritionStacks.TryGetValue(nutritionProps.FoodCategory, out var cur);
-        _nutritionStacks[nutritionProps.FoodCategory] = cur + adjustedStackSize;
-        MarkDirty(true);
+        _nutritionStacks[nutritionProps.FoodCategory] = cur + adjustedAccept;
         
-        accepted = actualStackSize;
+        accepted = adjustedAccept * ratio;
         return true;
+    }
+
+    private bool TryAddBrowns(ItemSlot slot, out int accepted)
+    {
+        accepted = 0;
+        int room = MAX_STACK_SIZE - _brownsQty;
+        if (room < 1
+        ||  slot.Itemstack?.Item?.Code != "drygrass"
+            )
+            return false;
+        
+        accepted = slot.StackSize > room 
+        ?   room
+        :   slot.StackSize;
+
+        _brownsQty += accepted;
+        
+        return true;
+    }
+
+    private bool TryAddInoculum(ItemSlot slot, out int accepted)
+    {
+        accepted = 0;
+        int room = MAX_STACK_SIZE - _inoculumQty;
+        if(room < 1)
+            return false;
+
+        string code = slot.Itemstack?.Item?.Code;
+        if (code is null)
+            return false;
+        
+        if(code == "compost")
+        {
+            accepted = slot.StackSize > room 
+            ?   room
+            :   slot.StackSize;
+
+            _inoculumQty += accepted;
+            return true;
+        }
+
+        if (code == "sourcompost"
+        ||  code == "rot"
+           )
+        {
+            int ratio = code == "sourcompost" ? SOUR_PER_INOCULUM : ROT_PER_INOCULUM;
+            
+            if (slot.StackSize < ratio)
+                return false;
+                
+            int adjustedStackSize = slot.StackSize / ratio;
+            int adjustedAccept = adjustedStackSize > room 
+            ?   room
+            :   adjustedStackSize;
+
+            _inoculumQty += adjustedAccept;
+            accepted = adjustedAccept * ratio;
+            return true;
+        }
+
+        return false;
     }
 
     public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldAccessForResolve)
     {
         base.FromTreeAttributes(tree, worldAccessForResolve);
 
+        _brownsQty = tree.GetInt("_brownsQty");
+        _inoculumQty = tree.GetInt("_inoculumQty");
+        
         if(_nutritionStacks is null)
             _nutritionStacks = new();
+        else
+            _nutritionStacks.Clear();
 
         int nutritionLength = tree.GetInt("_nutritionStacks.Count");
         for (int i = 0; i < nutritionLength; i++)
-            _nutritionStacks.Add((EnumFoodCategory)tree.GetInt($"_nutritionStacks<{i}>"),tree.GetInt($"_nutritionStacks[{i}]"));
+            _nutritionStacks[(EnumFoodCategory)tree.GetInt($"_nutritionStacks<{i}>")] = tree.GetInt($"_nutritionStacks[{i}]");
     }
 
     public override void ToTreeAttributes(ITreeAttribute tree)
     {
         base.ToTreeAttributes(tree);
 
+        tree.SetInt("_brownsQty", _brownsQty);
+        tree.SetInt("_inoculumQty", _inoculumQty);
+        
         if (_nutritionStacks is not null)
         {
             tree.SetInt("_nutritionStacks.Count", _nutritionStacks.Count);
