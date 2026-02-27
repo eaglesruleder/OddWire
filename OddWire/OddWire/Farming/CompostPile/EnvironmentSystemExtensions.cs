da43bec40d1cea653d5a0f0c473bb038550a4679
eaglesruleder <44625326+eaglesruleder@users.noreply.github.com>
2026-02-27 21:43:01 +0800

    Cleanup

--- OddWire/OddWire/Farming/CompostPile/EnvironmentSystemExtensions.cs
+++ OddWire/OddWire/Farming/CompostPile/EnvironmentSystemExtensions.cs
@@ -9,33 +9,34 @@ public static class EnvironmentSystemExtensions
     public static bool IsSkyExposed(this IBlockAccessor blockAccessor, BlockPos pos)
         => blockAccessor.GetRainMapHeightAt(pos.X, pos.Z) <= pos.Y;
 
-    public static ClimateCondition GetClimateAtHours(this IWorldAccessor world, BlockPos pos, double totalHours)
-        => world.BlockAccessor.GetClimateAt(
-            pos,
-            EnumGetClimateMode.ForSuppliedDate_TemperatureRainfallOnly,
-            totalHours / world.Calendar.HoursPerDay
-        );
+    public static ClimateCondition GetClimateAtHours(this IWorldAccessor world, BlockPos pos, double totalHours) =>
+        world.BlockAccessor.GetClimateAt
+            (pos
+            ,EnumGetClimateMode.ForSuppliedDate_TemperatureRainfallOnly
+            ,totalHours / world.Calendar.HoursPerDay
+            );
 
-    public static float GetEnvironmentTemperatureC(
-        this ICoreAPI api,
-        BlockPos pos,
-        double totalHours,
-        bool skyExposed,
-        float greenhouseTempBonusC,
-        out bool inGreenhouse
-    )
+    public static float GetEnvironmentTemperatureC
+        (this ICoreAPI api
+        ,BlockPos pos
+        ,double totalHours
+        ,bool skyExposed
+        ,float greenhouseTempBonusC
+        ,out bool inGreenhouse
+        )
     {
         inGreenhouse = false;
 
         ClimateCondition conds = api.World.GetClimateAtHours(pos, totalHours);
         float temp = conds?.Temperature ?? 0f;
-
+        
         if (!skyExposed)
         {
             var room = api.ModLoader.GetModSystem<RoomRegistry>()?.GetRoomForPosition(pos.UpCopy());
             if (room != null
-                && room.SkylightCount > room.NonSkylightCount
-                && room.ExitCount == 0)
+            &&  room.SkylightCount > room.NonSkylightCount
+            &&  room.ExitCount == 0
+                )
             {
                 inGreenhouse = true;
                 temp += greenhouseTempBonusC;
