ecc7e604ee81a0a3ad19a4056116753e3ac17122
eaglesruleder <44625326+eaglesruleder@users.noreply.github.com>
2026-05-14 09:41:59 +0800

    InHand Basket and Boulder Pickup (#11)
    * Initial change
    
    * Cleanup
    
    * Correction
    
    * Mark off ToDo
    
    * Allow boulder pickup

---
+++ OddWire/OddWire/Farming/_Patches/ItemSlotSurvival.patch.cs
@@ -0,0 +1,43 @@
+using Vintagestory.API.Common;
+using HarmonyLib;
+
+namespace OddWire.Patches;
+
+[HarmonyPatch(typeof(ItemSlotSurvival), nameof(ItemSlotSurvival.CanHold))]
+static class ItemSlotSurvival_CanHold_AllowSmallBag_Patch
+{
+    static bool Prefix(ItemSlot sourceSlot, ref bool __result, ItemSlotSurvival __instance)
+    {
+        var bag = sourceSlot.Itemstack?.Collectible.GetCollectibleInterface<IHeldBag>();
+        if (bag?.IsEmpty(sourceSlot.Itemstack) == true)
+            return true;
+
+        if (__instance is ItemSlotBagContent)
+            return false;
+        
+        var quantitySlots = bag?.GetQuantitySlots(sourceSlot.Itemstack);
+        if (quantitySlots >= 4)
+            return true;
+        
+        __result = __instance.Inventory.CanContain(__instance, sourceSlot);
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(ItemSlotSurvival), nameof(ItemSlotSurvival.CanTakeFrom))]
+static class ItemSlotSurvival_CanTakeFrom_AllowSmallBag_Patch
+{
+    static bool Prefix(ItemSlot sourceSlot, ref bool __result)
+    {
+        var bag = sourceSlot.Itemstack?.Collectible.GetCollectibleInterface<IHeldBag>();
+        if (bag?.IsEmpty(sourceSlot.Itemstack) == true)
+            return true;
+
+        var quantitySlots = bag?.GetQuantitySlots(sourceSlot.Itemstack);
+        if (quantitySlots >= 4)
+            return true;
+        
+        __result = true;
+        return false;
+    }
+}
