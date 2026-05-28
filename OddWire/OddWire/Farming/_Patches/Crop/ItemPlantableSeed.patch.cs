27ccc6ec98ae398a9a1abe8f1590d56babfb62e3
eaglesruleder <44625326+eaglesruleder@users.noreply.github.com>
2026-05-28 10:58:59 +0800

    Initial plot-variant crops

--- OddWire/OddWire/Farming/_Patches/Crop/ItemPlantableSeed.patch.cs
+++ OddWire/OddWire/Farming/_Patches/Crop/ItemPlantableSeed.patch.cs
@@ -1,49 +1,111 @@
+using System;
 using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.GameContent;
 using HarmonyLib;
 using OddWire.GameContent;
+using Vintagestory.API.Util;
 
 namespace OddWire.Patches;
 
-[HarmonyPatch(typeof(ItemPlantableSeed), "OnHeldInteractStart")]
-public static class ItemPlantableSeed_OnHeldInteractStart_PlantICropland_Patch
+// Intent: This patch file effectively rewrites ItemPlantableSeed
+// Outcome: Functionally similar, delegating to Farmland ?? ICropland
+
+[HarmonyPatch(typeof(ItemPlantableSeed), nameof(ItemPlantableSeed.OnLoaded))]
+public static class ItemPlantableSeed_OnLoaded_Overwrite_Patch
 {
+    private static readonly FieldInfo InteractionsField =
+        AccessTools.Field(typeof(ItemPlantableSeed), "interactions");
+
+    public static bool Prefix(ICoreAPI api, ItemPlantableSeed __instance)
+    {
+        if (api.Side != EnumAppSide.Client)
+            return false;
+
+        InteractionsField.SetValue(__instance, ObjectCacheUtil.GetOrCreate(api, "seedInteractions", () =>
+        {
+            List<ItemStack> stacks = new();
+            foreach (Block block in api.World.Blocks)
+            {
+                if (block.Code is null
+                ||  block.EntityClass is null
+                    )
+                    continue;
+
+                Type? beType = api.World.ClassRegistry.GetBlockEntity(block.EntityClass);
+                if (beType == typeof(BlockEntityFarmland)
+                ||  beType?.IsAssignableTo(typeof(ICropland)) == true
+                   )
+                    stacks.Add(new ItemStack(block));
+            }
+
+            return new WorldInteraction[]
+                {new()
+                    {ActionLangCode = "heldhelp-plant"
+                    ,MouseButton    = EnumMouseButton.Right
+                    ,Itemstacks     = stacks.ToArray()
+                    }
+                };
+        }));
+
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(ItemPlantableSeed), nameof(ItemPlantableSeed.OnHeldInteractStart))]
+public static class ItemPlantableSeed_OnHeldInteractStart_Overwrite_Patch
+{
+    [HarmonyReversePatch]
+    [HarmonyPatch(typeof(CollectibleObject), nameof(CollectibleObject.OnHeldInteractStart))]
+    public static void base_OnHeldInteractStart
+        (ItemPlantableSeed instance
+        ,ItemSlot itemslot, EntityAgent byEntity, BlockSelection blockSel ,EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handHandling
+        ) => throw new NotImplementedException("Harmony reverse patch stub");
+    
     public static bool Prefix
-        (ItemPlantableSeed __instance
-        ,ItemSlot itemslot
-        ,EntityAgent byEntity
-        ,BlockSelection blockSel
-        ,EntitySelection entitySel
-        ,bool firstEvent
-        ,ref EnumHandHandling handHandling
+        (ItemSlot itemslot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handHandling
+        ,ItemPlantableSeed __instance
         )
     {
-        #region if(!blockSel is ICropland || blockSel is Farmland) return true;
-        if (blockSel is null)
-            return true;
-
-        if (byEntity.World.BlockAccessor.GetBlockEntity(blockSel.Position) is not ICropland target
-        ||  target is BlockEntityFarmland
+        #region if(!blockSel || !cropType || BE is not BEFarmland or ICropland) return false
+        string? cropType = itemslot.Itemstack?.Collectible?.Variant?["type"];
+        if (blockSel is null
+        ||  cropType is null
            )
-            return true;
+        {
+            base_OnHeldInteractStart(__instance, itemslot, byEntity, blockSel, entitySel, firstEvent, ref handHandling);
+            return false;
+        }
+        
+        Block b = byEntity.World.BlockAccessor.GetBlock(blockSel.Position);
+        BlockEntity? be = byEntity.World.BlockAccessor.GetBlockEntity(blockSel.Position);
+        if (be is not BlockEntityFarmland
+        &&  be is not ICropland
+           )
+        {
+            base_OnHeldInteractStart(__instance, itemslot, byEntity, blockSel, entitySel, firstEvent, ref handHandling);
+            return false;
+        }
         #endregion
 
-        #region if(!Variant["type"] || !GetBlock("crop-{croptype}-1") return false;
-        string? croptype = itemslot.Itemstack?.Collectible?.Variant?["type"];
-        if (croptype is null)
-            return false;
-
-        Block cropBlock = byEntity.World.GetBlock(__instance.CodeWithPath($"crop-{croptype}-1"));
+        #region if(!cropBlock || !be.TryPlant()) return false;
+        string plotType = be.Block.Code.FirstCodePart();
+        Block? cropBlock = byEntity.World.GetBlock(__instance.CodeWithPath($"crop-{cropType}-{plotType}-1"));
         if (cropBlock is null)
             return false;
-        #endregion
         
-        IPlayer? byPlayer = (byEntity as EntityPlayer)?.Player;
-        if (!target.TryPlant(cropBlock, itemslot, byEntity, blockSel))
+        bool planted =
+            (be as BlockEntityFarmland)?.TryPlant(cropBlock, itemslot, byEntity, blockSel)
+        ??  (be as ICropland)!.TryPlant(cropBlock, itemslot, byEntity, blockSel);
+        if (!planted)
             return false;
+        #endregion
 
+        IPlayer? byPlayer = (byEntity as EntityPlayer)?.Player;
         byEntity.World.PlaySoundAt(new AssetLocation("sounds/block/plant"), blockSel.Position, 0.4375, byPlayer);
         (byPlayer as IClientPlayer)?.TriggerFpAnimation(EnumHandInteract.HeldItemInteract);
 
@@ -58,43 +120,45 @@ public static class ItemPlantableSeed_OnHeldInteractStart_PlantICropland_Patch
     }
 }
 
-[HarmonyPatch(typeof(ItemPlantableSeed), nameof(ItemPlantableSeed.OnLoaded))]
-public static class ItemPlantableSeed_OnLoaded_Patch
+[HarmonyPatch(typeof(ItemPlantableSeed), nameof(ItemPlantableSeed.GetHeldItemInfo))]
+public static class ItemPlantableSeed_GetHeldItemInfo_Overwrite_Patch
 {
-    public static void Postfix(ItemPlantableSeed __instance, ICoreAPI api)
+    [HarmonyReversePatch]
+    [HarmonyPatch(typeof(Item), nameof(Item.GetHeldItemInfo))]
+    public static void base_GetHeldItemInfo
+        (ItemPlantableSeed instance
+        ,ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo
+        ) => throw new NotImplementedException("Harmony reverse patch stub");
+
+    public static bool Prefix
+        (ItemSlot inSlot, StringBuilder dsc, IWorldAccessor world, bool withDebugInfo
+        ,ItemPlantableSeed __instance
+        )
     {
-        if (api.Side != EnumAppSide.Client)
-            return;
-
-        if (AccessTools.Field(typeof(ItemPlantableSeed), "interactions")
-                .GetValue(__instance) is not WorldInteraction[] interactions
-        ||  interactions.Length == 0
-           )
-            return;
-
-        // Guard: Only add plowland stacks once
-        if (interactions[0].Itemstacks != null)
-        {
-            foreach (ItemStack s in interactions[0].Itemstacks)
-                if (s.Block is BlockPlowland)
-                    return;
-        }
-
-        List<ItemStack> extra = new();
-        foreach (Block block in api.World.Blocks)
-        {
-            if (block?.Code is null || block.EntityClass is null)
-                continue;
-            
-            if (api.World.ClassRegistry.GetBlockEntity(block.EntityClass) == typeof(BlockEntityPlowland))
-                extra.Add(new ItemStack(block));
-        }
-
-        if (extra.Count == 0)
-            return;
+        base_GetHeldItemInfo(__instance, inSlot, dsc, world, withDebugInfo);
         
-        List<ItemStack> merged = new(interactions[0].Itemstacks ?? []);
-        merged.AddRange(extra);
-        interactions[0].Itemstacks = merged.ToArray();
+        #region cropProps = GetBlock("crop-{cropType}-farmland-1")?.CropProps ?? return false;
+        string? cropType = inSlot.Itemstack?.Collectible?.Variant?["type"];
+        if (cropType is null)
+            return false;
+
+        var cropProps = world.GetBlock(__instance.CodeWithPath($"crop-{cropType}-farmland-1"))?.CropProps;
+        if (cropProps is null)
+            return false;
+        #endregion
+        
+        dsc.AppendLine(Lang.Get("soil-nutrition-requirement") + cropProps.RequiredNutrient);
+        dsc.AppendLine(Lang.Get("soil-nutrition-consumption") + cropProps.NutrientConsumption);
+
+        double totalDays = cropProps.TotalGrowthDays > 0
+        ?   cropProps.TotalGrowthDays / 12
+        :   cropProps.TotalGrowthMonths;
+        totalDays *= world.Calendar.DaysPerMonth / world.Config.GetDecimal("cropGrowthRateMul", 1);
+        dsc.AppendLine(Lang.Get("soil-growth-time") + " " + Lang.Get("count-days", Math.Round(totalDays, 1)));
+        
+        dsc.AppendLine(Lang.Get("crop-coldresistance", Math.Round(cropProps.ColdDamageBelow, 1)));
+        dsc.AppendLine(Lang.Get("crop-heatresistance", Math.Round(cropProps.HeatDamageAbove, 1)));
+
+        return false;
     }
 }
